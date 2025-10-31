255a43af4f8cbbb5686feccf9e69bb853cf6ea91
janardhanmerugu <janardhanmerugu@gmail.com>
2025-10-31 14:55:12 +0530

    Update dhan_api.py

--- dhan_api.py
+++ dhan_api.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-ACCESS_TOKEN = os.getenv("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkaGFuIiwicGFydG5lcklkIjoiIiwiZXhwIjoxNzYxOTg3MDc3LCJpYXQiOjE3NjE5MDA2NzcsInRva2VuQ29uc3VtZXJUeXBlIjoiU0VMRiIsIndlYmhvb2tVcmwiOiIiLCJkaGFuQ2xpZW50SWQiOiIxMTA5MDIwNDgyIn0.kUg6PuC7YT9KfrL7DoCC7oHwCHKk8wmCsFFa5NO8on8qXyd6Z37z1r058dyvRHyt6QesOi8kF2B-xK4HyDzW9g")
-CLIENT_ID = os.getenv("1109020482")
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+CLIENT_ID = os.getenv("CLIENT_ID")
 BASE_URL = "https://api.dhan.co/v2"
 
 def get_banknifty_quote():
