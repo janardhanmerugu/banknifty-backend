23b410b0f84d4df78279c41f40cfc47aa2f04aa3
janardhanmerugu <janardhanmerugu@gmail.com>
2025-10-31 14:23:30 +0530

    Change ACCESS_TOKEN to new token value
    Updated ACCESS_TOKEN to use a new token value.

--- dhan_api.py
+++ dhan_api.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-ACCESS_TOKEN = os.getenv("DHAN_ACCESS_TOKEN")
+ACCESS_TOKEN = os.getenv("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkaGFuIiwicGFydG5lcklkIjoiIiwiZXhwIjoxNzYxOTg3MDc3LCJpYXQiOjE3NjE5MDA2NzcsInRva2VuQ29uc3VtZXJUeXBlIjoiU0VMRiIsIndlYmhvb2tVcmwiOiIiLCJkaGFuQ2xpZW50SWQiOiIxMTA5MDIwNDgyIn0.kUg6PuC7YT9KfrL7DoCC7oHwCHKk8wmCsFFa5NO8on8qXyd6Z37z1r058dyvRHyt6QesOi8kF2B-xK4HyDzW9g")
 CLIENT_ID = os.getenv("DHAN_CLIENT_ID")
 BASE_URL = "https://api.dhan.co/v2"
 
